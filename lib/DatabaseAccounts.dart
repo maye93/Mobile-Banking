e2093c57e24d9a3b52bfa318649b943f5b78209b
unknown <shaniamitsui@gmail.com>
2021-10-06 17:34:19 +0800

    updated info

--- lib/DatabaseAccounts.dart
+++ lib/DatabaseAccounts.dart
@@ -1,1 +1,17 @@
+class AdminAccount {
+  String _privAcc = "admin";
+  String _privPass = "admin";
+  int _privBalance = 100000;
 
+  account() {
+    return _privAcc;
+  }
+
+  password() {
+    return _privPass;
+  }
+
+  balance() {
+    return _privBalance;
+  }
+}
