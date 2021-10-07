0df4e27c87e1beeaa4dd3a6ea82488ba3c5323bd
occasionalcode <occasionalprogrammer@gmail.com>
2021-10-07 21:18:12 +0800

    Confirmation test next return

---
+++ lib/Confirmation.dart
@@ -0,0 +1,50 @@
+/// Flutter code sample for Stepper.controlsBuilder
+
+// Creates a stepper control with custom buttons.
+
+import 'package:flutter/material.dart';
+
+// /// This is the stateless widget that the main application instantiates.
+class Confirmation extends StatelessWidget {
+  const Confirmation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Stepper(
+      controlsBuilder: (BuildContext context,
+          {VoidCallback? onStepContinue, VoidCallback? onStepCancel}) {
+        return Row(
+          children: <Widget>[
+            TextButton(
+              onPressed: onStepContinue,
+              child: const Text('NEXT'),
+            ),
+            TextButton(
+              onPressed: onStepCancel,
+              child: const Text('EXIT'),
+            ),
+          ],
+        );
+      },
+      steps: const <Step>[
+        Step(
+          title: Text('A'),
+          content: SizedBox(
+            width: 100.0,
+            height: 100.0,
+          ),
+        ),
+        Step(
+          title: Text('B'),
+          content: SizedBox(
+            width: 100.0,
+            height: 100.0,
+            child: Text("hello"),
+          ),
+        ),
+      ],
+    )));
+  }
+}
